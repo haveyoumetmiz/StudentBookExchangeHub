b44c5f7d485b4f582872c90d873c07094656c4e5
Mizhab Ansar <130668310+haveyoumetmiz@users.noreply.github.com>
2024-03-07 05:23:55 +0000

    ed

--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 { pkgs }: {
     deps = [
+      pkgs.run
       pkgs.python39Packages.django
       pkgs.nodePackages.vscode-langservers-extracted
       pkgs.nodePackages.typescript-language-server
